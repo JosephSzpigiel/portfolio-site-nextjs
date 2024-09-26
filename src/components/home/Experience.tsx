dc28aeaa8d22843fa01441b5328ce14b18015f4f
Joseph Szpigiel <josephszpigiel@gmail.com>
2024-09-26 10:51:10 -0400

    Added styling and content to Projects

--- src/components/home/Experience.tsx
+++ src/components/home/Experience.tsx
@@ -3,7 +3,9 @@ import { Box, Link, Text, Badge, Stack } from '@chakra-ui/react'
 
 const Experience = () => {
   return (
-    <>
+    <Box
+        paddingBottom={'50px'}
+    >
         <Text
             fontSize={'24px'}
             fontWeight={'500'}
@@ -47,41 +49,41 @@ const Experience = () => {
         // border={"2px solid red"}
         display={'flex'}
         flexDirection={['column', 'row', 'row']}
-    >
-        <Box
-            // border={"2px solid purple"}
-            width={'30%'}
         >
-            2018 — 2024
-        </Box>
-        <Box
-            // border={"2px solid yellow"}
-            width={['100%', '70%']}
-        >
-            <Link 
-                href='https://upstatement.com/' 
-                isExternal
-                fontSize={'24px'}
-                fontWeight={'500'}
-                >
-                Lead Engineer · Upstatement
-            </Link>
-            <Text>
-                Build, style, and ship high-quality websites, design systems, mobile apps, and digital experiences for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and spearheading the development of internal tools.
-            </Text>
-            <Stack direction={'row'} wrap={'wrap'}>
-                <Badge colorScheme='green'>Javascript</Badge>
-                <Badge colorScheme='green'>Typescript</Badge>
-                <Badge colorScheme='green'>React</Badge>
-                <Badge colorScheme='green'>CSS</Badge>
-                <Badge colorScheme='green'>HTML</Badge>
-                <Badge colorScheme='green'>JSX</Badge>
-                <Badge colorScheme='green'>Next.js</Badge>
-                <Badge colorScheme='green'>Chakra UI</Badge>
-            </Stack>
+            <Box
+                // border={"2px solid purple"}
+                width={'30%'}
+            >
+                2018 — 2024
+            </Box>
+            <Box
+                // border={"2px solid yellow"}
+                width={['100%', '70%']}
+            >
+                <Link 
+                    href='https://upstatement.com/' 
+                    isExternal
+                    fontSize={'24px'}
+                    fontWeight={'500'}
+                    >
+                    Lead Engineer · Upstatement
+                </Link>
+                <Text>
+                    Build, style, and ship high-quality websites, design systems, mobile apps, and digital experiences for a diverse array of projects for clients including Harvard Business School, Everytown for Gun Safety, Pratt Institute, Koala Health, Vanderbilt University, The 19th News, and more. Provide leadership within engineering department through close collaboration, knowledge shares, and spearheading the development of internal tools.
+                </Text>
+                <Stack direction={'row'} wrap={'wrap'}>
+                    <Badge colorScheme='green'>Javascript</Badge>
+                    <Badge colorScheme='green'>Typescript</Badge>
+                    <Badge colorScheme='green'>React</Badge>
+                    <Badge colorScheme='green'>CSS</Badge>
+                    <Badge colorScheme='green'>HTML</Badge>
+                    <Badge colorScheme='green'>JSX</Badge>
+                    <Badge colorScheme='green'>Next.js</Badge>
+                    <Badge colorScheme='green'>Chakra UI</Badge>
+                </Stack>
+            </Box>
         </Box>
     </Box>
-    </>
     
   )
 }
--- src/components/home/Projects.tsx
+++ src/components/home/Projects.tsx
@@ -1,37 +1,40 @@
-import { Text, Link, Stack, Image, Card, CardBody, Heading, CardFooter, ButtonGroup, Button, HStack } from '@chakra-ui/react'
+import { Text, Link, Stack, Image, Card, CardBody, Heading, CardFooter, ButtonGroup, Button, HStack, Badge, Divider, Center } from '@chakra-ui/react'
 import React from 'react'
 
 function Projects() {
 
     type Project = {
-        link: string | undefined;
+        link?: string;
         description: string | undefined; 
         image: string | undefined; 
         name: string | undefined;
         github: string | undefined;
+        tech?: string[];
     }
 
     const projectDescriptions: Project[] = [
         {
-            link: 'https://virtualpoker-8bfvmdsrw-josephszpigiels-projects.vercel.app/',
-            description: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces, earthy toned spaces and for people who love a chic design with a sprinkle of vintage design.',
+            link: 'https://virtualpoker-josephszpigiels-projects.vercel.app/',
+            description: 'Make a bet and deal yourself in! This project utilizes the Deck of Cards API to mimic a real deck of cards and follows the rules of Virtual Poker.',
             image: 'images/VPoker.png',
-            name: 'Test',
-            github: 'https://github.com/JosephSzpigiel/vpoker-react'
+            name: 'Virtual Poker',
+            github: 'https://github.com/JosephSzpigiel/vpoker-react',
+            tech: ['Javascript', 'React', 'Next.js']
         },
         {
-            link: 'https://virtualpoker-8bfvmdsrw-josephszpigiels-projects.vercel.app/',
-            description: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces, earthy toned spaces and for people who love a chic design with a sprinkle of vintage design.',
-            image: 'images/VPoker.png',
-            name: 'Test 2',
-            github: 'https://github.com/JosephSzpigiel/vpoker-react'
+            description: 'Find a cocktail and relax with this project! Built in Vanilla Javascript and utilizing the CocktailDB API, you can find a drink recipe by name or ingredient and when run locally, save your favorites to a JSON server.',
+            image: 'images/DrinkFinder.png',
+            name: 'Drink Finder',
+            github: 'https://github.com/JosephSzpigiel/drink-finder',
+            tech: ['Javascript', 'HTML', 'CSS', 'API'],
+            link: 'https://josephszpigiel.github.io/drink-finder/'
         },
         {
-            link: 'https://virtualpoker-8bfvmdsrw-josephszpigiels-projects.vercel.app/',
-            description: 'This sofa is perfect for modern tropical spaces, baroque inspired spaces, earthy toned spaces and for people who love a chic design with a sprinkle of vintage design.',
-            image: 'images/VPoker.png',
-            name: 'Test 2',
-            github: 'https://github.com/JosephSzpigiel/vpoker-react'
+            description: 'Manage your Dungeons and Dragons games with this CLI! This project was built in Python utilizing a SQLite3 database. Try using the input "dm" with passcode "secret" to enter the hidden DM mode!',
+            image: 'images/DnD.png',
+            name: 'DnD Character Creator CLI',
+            github: 'https://github.com/peymere/dnd-campaign-manager-cli-app',
+            tech: ['Python', 'SQLite3', 'ORM', 'CLI']
         },
         {
             link: 'https://virtualpoker-8bfvmdsrw-josephszpigiels-projects.vercel.app/',
@@ -57,18 +60,31 @@ function Projects() {
 
     ]
 
+
     function ProjectCard(project: Project){
+        
+        let techBadges = null
+        
+        
+        if(project.tech){
+            techBadges = project.tech.map(tech => <Badge colorScheme='green'>{tech}</Badge>)
+        }
+
         return(
-            <Card maxW='sm' background={'#57647A'} textColor={'white'}>
+            <Card maxW='sm' h={'lg'} background={'#57647A'} textColor={'white'}>
                 <CardBody>
-                    <Image src={project.image} alt={project.name} borderRadius={'lg'}/>
+                    <Image src={project.image} alt={project.name} borderRadius={'lg'} w={'500px'} h={'175px'}/>
                     <Stack mt='6' spacing='3'>
                         <Heading size='md'>{project.name}</Heading>
                         <Text>
                             {project.description}
                         </Text>
+                        <Stack direction={'row'} wrap={'wrap'}>
+                            {techBadges}
+                        </Stack>
                     </Stack>
                 </CardBody>
+                <Divider/>
                 <CardFooter>
                     <ButtonGroup spacing='2'>
                     {project.link ? 
@@ -93,17 +109,23 @@ function Projects() {
             <Text
                 fontSize={'24px'}
                 fontWeight={'500'}
+                paddingBottom={'10px'}
             >
                 Projects
             </Text>
-                <HStack
-                    // border={"2px solid red"}
-                    display={'flex'}
-                    flexWrap={'wrap'}
-                    justifyContent={'center'}
-                >
-                    {ProjectCards}
-                </HStack>
+            <HStack
+                // border={"2px solid red"}
+                display={'flex'}
+                flexWrap={'wrap'}
+                justifyContent={'center'}
+            >
+                {ProjectCards}
+            </HStack>
+            <Center paddingTop={'10px'}>
+                <Link>
+                    <Button>See all projects</Button>
+                </Link>
+            </Center>
         </>
     )
 }
